f51ac33b5a453f656f9efe26e26a22b1572140cd
jrenau <dam.jrenau@institutoserpis.org>
2015-10-07 08:51:40 +0200

    JHolaMundo: cambiado para pedir el nombre y saludar

--- JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -1,8 +1,11 @@
 package org.institutoserpis.ed;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		Scanner	scanner = new Scanner(System.in);
 System.out.println("Hola Mundo desde Eclipse");
 	}
 

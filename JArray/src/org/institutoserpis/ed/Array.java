b4f17a9a433cb0e48a8a8afdb2a6f8c060b0e548
jrenau <dam.jrenau@institutoserpis.org>
2015-10-19 11:29:30 +0200

    Añadido JArray: resulto suma; propuestos menor y indexOf

---
+++ JArray/src/org/institutoserpis/ed/Array.java
@@ -0,0 +1,43 @@
+package org.institutoserpis.ed;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		
+		int[] v = new int[] {5, 9, 7, 12, 21};
+		
+		
+		for(int index=0; index < v.length; index++)
+		System.out.println("el elemento con index " + index + " vale " + v[index]);
+		
+		System.out.println("suma del array= " + suma(v));
+		System.out.println("suma del otro array= " + suma(new int[]{14, 24}));
+		System.out.println("suma del array vacio= " + suma(new int[]{}));
+	}
+	
+	private static int suma(int[] v){
+		//número de elementos del vector: v.length
+		int suma = 0;
+		
+//		for (int index=0; index < v.length; index++)
+//			suma = suma + v[index];
+		
+		for (int item : v)
+			suma = suma + item;
+		
+		return suma;
+	}
+	private static int menor(int[] v){
+		//TODO implemetar
+		int menor = v[0];
+		System.out.println("El valor menor de v es: " + menor(v));
+		return 0;
+	}
+	private static int indexOf(int[] v, int item) {
+		//TODO implemetar
+		System.out.println(" " + menor(v));
+		return item; 
+		
+}
+}
